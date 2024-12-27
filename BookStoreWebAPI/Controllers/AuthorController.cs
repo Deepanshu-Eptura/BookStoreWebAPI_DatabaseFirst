6fc5d187ecface4a2d4010ae0f9cb2f6ffdf0da9
Deepanshu Saini <deepanshu.saini@eptura.com>
2024-12-27 22:07:27 +0530

    Bug fixes

--- BookStoreWebAPI/Controllers/AuthorController.cs
+++ BookStoreWebAPI/Controllers/AuthorController.cs
@@ -17,16 +17,22 @@ namespace BookStoreWebAPI.Controllers
                 //return context.Author.ToList();
 
                 //add a Author
-                Author author = new Author();
-                author.FirstName = "Avi";
-                author.LastName = "Saini";
+                //Author author = new Author();
+                //author.FirstName = "Avi";
+                //author.LastName = "Saini";
 
 
-                context.Author.Add(author);
+                //context.Author.Add(author);
 
                 ////to update the author
                 //Author author = context.Author.Where(auth => auth.FirstName == "Avi").FirstOrDefault();
                 //author.Phone = "12346-77777";
+
+                // remove author
+                Author author = context.Author.Where(auth => auth.FirstName =="Avi").FirstOrDefault();
+                context.Author.Remove(author);
+
+
                 context.SaveChanges();
 
                 ////to get author by id
